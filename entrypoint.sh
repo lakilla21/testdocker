fb8ad80043ca29291ca81cb8d659b51c3a76afc0
lakilla21 <lakilla21@gmail.com>
2020-03-06 11:49:26 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -41,4 +41,4 @@ MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-${MODIFIED_STARTUP}
+#${MODIFIED_STARTUP}
